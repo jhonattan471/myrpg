0ef7a5b7eebafee687202ef63147cdc9020b241a
Jhonattan Oliveira <jhonattan.oliveira@gestran.com.br>
2025-04-07 20:55:15 -0300

    inventario sem drag and drop

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,87 +1,87 @@
-import { TestBed } from '@angular/core/testing';
-import { AppComponent } from './app.component';
-import { Objeto } from './models/objeto';
-import { Mundo } from './models/mundo';
-import { NotPossibleError } from './models/errors';
-import * as THREE from 'three';
-import { Posicao } from './models/posicao';
+// import { TestBed } from '@angular/core/testing';
+// import { AppComponent } from './app.component';
+// import { Objeto } from './models/objeto';
+// import { Mundo } from './models/mundo';
+// import { NotPossibleError } from './models/errors';
+// import * as THREE from 'three';
+// import { Posicao } from './models/posicao';
 
-describe('AppComponent', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [AppComponent],
-    }).compileComponents();
-  });
+// describe('AppComponent', () => {
+//   beforeEach(async () => {
+//     await TestBed.configureTestingModule({
+//       imports: [AppComponent],
+//     }).compileComponents();
+//   });
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
+//   it('should create the app', () => {
+//     const fixture = TestBed.createComponent(AppComponent);
+//     const app = fixture.componentInstance;
+//     expect(app).toBeTruthy();
+//   });
 
-  it('Deve iniciar o espaço 3D', () => {
-    const mundo = new Mundo()
-    expect(mundo.getScene()).toBeDefined();
-    expect(mundo.getScene() instanceof THREE.Scene).toBeTrue();
-  });
+//   it('Deve iniciar o espaço 3D', () => {
+//     const mundo = new Mundo()
+//     expect(mundo.getScene()).toBeDefined();
+//     expect(mundo.getScene() instanceof THREE.Scene).toBeTrue();
+//   });
 
-  it('Deve criar um objeto no mundo', () => {
-    const mundo = new Mundo()
+//   it('Deve criar um objeto no mundo', () => {
+//     const mundo = new Mundo()
 
-    const object = mundo.adicionarObjeto(new Objeto());
-    expect(object).toBeDefined();
-    expect(mundo.getScene().children.includes(object)).toBeTrue();
-  });
+//     const object = mundo.adicionarObjeto(new Objeto());
+//     expect(object).toBeDefined();
+//     expect(mundo.getScene().children.includes(object)).toBeTrue();
+//   });
 
-  it(`Dois corpos não podem ser criados no mesmo espaço`, () => {
-    const mundo = new Mundo();
-    const objeto1 = new Objeto();
-    const objeto2 = new Objeto();
+//   it(`Dois corpos não podem ser criados no mesmo espaço`, () => {
+//     const mundo = new Mundo();
+//     const objeto1 = new Objeto();
+//     const objeto2 = new Objeto();
 
-    objeto1.posicao = { x: 0, y: 0, z: 0 };
-    objeto2.posicao = { x: 0, y: 0, z: 0 };
+//     objeto1.posicao = { x: 0, y: 0, z: 0 };
+//     objeto2.posicao = { x: 0, y: 0, z: 0 };
 
-    mundo.adicionarObjeto(objeto1);
+//     mundo.adicionarObjeto(objeto1);
 
-    expect(() => mundo.adicionarObjeto(objeto2)).toThrowError(NotPossibleError);
-  });
-
-
-  it(`Dois corpos não podem ser movimentados no mesmo espaço`, () => {
-    const mundo = new Mundo();
-    const objeto1 = new Objeto();
-    const objeto2 = new Objeto();
-
-    objeto1.posicao = { x: 0, y: 0, z: 0 };
-    objeto2.posicao = { x: 0, y: 1, z: 0 };
-
-    mundo.adicionarObjeto(objeto1);
-    mundo.adicionarObjeto(objeto2);
-    mundo.movimentarPlayer()
-    expect(() => objeto2.mover({ x: 0, y: 0, z: 0 }, mundo)).toThrowError(NotPossibleError);
-  });
-
-  it(`Dois corpos podem ser criados em locais diferentes`, () => {
-    const mundo = new Mundo();
-    const objeto1 = new Objeto();
-    const objeto2 = new Objeto();
-
-    objeto1.posicao = { x: 0, y: 0, z: 0 };
-    objeto2.posicao = { x: 0, y: 1, z: 0 };
-
-    mundo.adicionarObjeto(objeto1);
-    mundo.adicionarObjeto(objeto2);
-
-    expect(mundo.objetos.length).toBe(2);
-    expect(objeto1.posicao).not.toEqual(objeto2.posicao);
-    expect(objeto1.posicao).toEqual({ x: 0, y: 0, z: 0 });
-    expect(objeto2.posicao).toEqual({ x: 0, y: 1, z: 0 });
-  });
+//     expect(() => mundo.adicionarObjeto(objeto2)).toThrowError(NotPossibleError);
+//   });
 
 
+//   it(`Dois corpos não podem ser movimentados no mesmo espaço`, () => {
+//     const mundo = new Mundo();
+//     const objeto1 = new Objeto();
+//     const objeto2 = new Objeto();
 
-  // it(`Dois corpos não podem ser movidos para o mesmo espaço`, () => {
-  // });
+//     objeto1.posicao = { x: 0, y: 0, z: 0 };
+//     objeto2.posicao = { x: 0, y: 1, z: 0 };
 
-});
+//     mundo.adicionarObjeto(objeto1);
+//     mundo.adicionarObjeto(objeto2);
+//     mundo.movimentarPlayer()
+//     expect(() => objeto2.mover({ x: 0, y: 0, z: 0 }, mundo)).toThrowError(NotPossibleError);
+//   });
+
+//   it(`Dois corpos podem ser criados em locais diferentes`, () => {
+//     const mundo = new Mundo();
+//     const objeto1 = new Objeto();
+//     const objeto2 = new Objeto();
+
+//     objeto1.posicao = { x: 0, y: 0, z: 0 };
+//     objeto2.posicao = { x: 0, y: 1, z: 0 };
+
+//     mundo.adicionarObjeto(objeto1);
+//     mundo.adicionarObjeto(objeto2);
+
+//     expect(mundo.objetos.length).toBe(2);
+//     expect(objeto1.posicao).not.toEqual(objeto2.posicao);
+//     expect(objeto1.posicao).toEqual({ x: 0, y: 0, z: 0 });
+//     expect(objeto2.posicao).toEqual({ x: 0, y: 1, z: 0 });
+//   });
+
+
+
+//   // it(`Dois corpos não podem ser movidos para o mesmo espaço`, () => {
+//   // });
+
+// });
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { AfterViewInit, Component } from '@angular/core';
 import { Mundo } from './models/mundo';
 import { Monstro, Objeto } from './models/objeto';
+import { Inventario } from './models/inventario.js';
 
 @Component({
   selector: 'app-root',
@@ -10,11 +11,21 @@ import { Monstro, Objeto } from './models/objeto';
 export class AppComponent implements AfterViewInit {
 
   mundo!: Mundo
+  divInventario
+  inventario = new Inventario(20);
+
+
   ngAfterViewInit(): void {
+    this.divInventario = document.getElementById('inventario') as any;
     this.mundo = new Mundo()
     this.mundo.adicionarPlayer()
-    // this.adicionarObjeto1()
     this.adicionarMonstro()
+
+    this.inventario.adicionarItem({ icon: `/gold.png` });
+    this.inventario.adicionarItem({ icon: `/pocao-vida.png` });
+    this.inventario.adicionarItem({ icon: `/pocao-mana.png` });
+
+    this.renderInventario()
   }
 
   adicionarObjeto1() {
@@ -25,5 +36,37 @@ export class AppComponent implements AfterViewInit {
   adicionarMonstro() {
     const monstro = new Monstro(0, -5, 0)
     this.mundo.adicionarObjeto(monstro)
+  }
+
+  renderInventario() {
+    this.inventario.slots.forEach((item, index) => {
+      const slot = document.createElement('div');
+      slot.classList.add('slot');
+      slot.classList.add('center-flex');
+      slot.dataset['index'] = index + '';
+
+      if (item) {
+        const img = document.createElement('img');
+        img.src = item.icon; // `icon` é uma URL do item, defina no seu modelo
+        img.draggable = true;
+        img.ondragstart = e => {
+          e.dataTransfer?.setData("text/plain", index + '');
+        };
+        slot.appendChild(img);
+      }
+
+      slot.ondragover = e => e.preventDefault();
+      slot.ondrop = e => {
+        const fromIndex = parseInt(e.dataTransfer?.getData("text/plain") || '0');
+        const toIndex = parseInt(slot.dataset['index'] || '0');
+        this.inventario.moverItem(fromIndex, toIndex);
+        this.renderInventario();
+      };
+      console.log("!!")
+      console.log(slot, this.divInventario)
+
+      this.divInventario.appendChild(slot);
+
+    });
   }
 }
